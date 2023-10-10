69c11bc9e5e5df3b9184bfa8d98c5bdc39c5def6
Maxim Lisin <lisin.maxim.m@gmail.com>
2023-10-10 21:33:26 +0300

    lab2 has been done

--- src/main/java/ru/lisin/rsa/Main.java
+++ src/main/java/ru/lisin/rsa/Main.java
@@ -1,7 +1,39 @@
 package ru.lisin.rsa;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
 
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            String inputMessage = "This is a test message that I need to encrypt and decrypt :D";
+            System.out.println("Input message: " + inputMessage);
+
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedBytes = cipher.doFinal(inputMessage.getBytes());
+            String encryptedMessage = new String(encryptedBytes);
+            System.out.println("Encrypted message: " + encryptedMessage);
+
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            System.out.println("Decrypted message: " + new String(decryptedBytes));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
